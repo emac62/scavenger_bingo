524156c868d35aaba2c1b580f5be8bcb648b7373
emac62 <borderlineboomer@gmail.com>
2023-03-05 12:20:43 -0500

    ios build 4 final

--- lib/settings.dart
+++ lib/settings.dart
@@ -348,7 +348,7 @@ class _SettingsPageState extends State<SettingsPage> {
                 Padding(
                   padding: const EdgeInsets.fromLTRB(10, 0, 10, 8),
                   child: AutoSizeText(
-                    "Choose the same card and the same way to win. Click 'Share' to send an image of your winning card. Only available for cards without images.",
+                    "Choose the same location and the same way to win. Click 'Share' to send an image of your winning card. Only available for cards without images.",
                     textAlign: TextAlign.justify,
                     style: TextStyle(
                       color: Colors.purple,
